9f7b41458d2c5b5ac133bea05e5e00e2c7e686f4
msmkdenis <msmkdenis@gmail.com>
2023-06-19 21:27:50 +0300

    <refactor>: замена forkjoinpool на cachedthreadpool, скорректировано создание экзекьютора

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -11,13 +11,15 @@ import static java.lang.Double.NaN;
 public class PriceAggregator {
 
     private PriceRetriever priceRetriever = new PriceRetriever();
+
+    //Так как предполагается параллелить http запросы выгоднее использовать CachedThreadPool
+    private final Executor executor = Executors.newCachedThreadPool();
+
     private static final long PRICE_RETRIEVE_TIMEOUT = 2_900L;
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
         this.priceRetriever = priceRetriever;
     }
-
-    ExecutorService executorService = Executors.newCachedThreadPool();
 
     private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
 
@@ -32,14 +34,12 @@ public class PriceAggregator {
         *  В следствии указанного настройки forkjoinpool по умолчанию не подходят для
         *  большого количество I/O задач с необходимым тайм-аутом исполнения.
         *
-        *  Для того чтобы все тесты успешно завершились дефолтный forkjoinpool был скорерктирован.        *
-        * */
-
-        Executor forkJoinPool = forkJoinPool();
+        *  Для того чтобы все тесты успешно завершились дефолтный forkjoinpool был заменен на cachedthreadpool
+        */
 
         List<CompletableFuture<Double>> minShopPrices = shopIds.stream()
                 .map(shopId -> CompletableFuture
-                        .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), forkJoinPool)
+                        .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
                         .exceptionally(e -> Double.NaN)
                         .completeOnTimeout(NaN, PRICE_RETRIEVE_TIMEOUT, TimeUnit.MILLISECONDS))
                 .collect(Collectors.toList());
@@ -49,20 +49,5 @@ public class PriceAggregator {
                 .filter(price-> !price.isNaN())
                 .min(Double::compareTo)
                 .orElse(NaN);
-    }
-
-    private ForkJoinPool forkJoinPool() {
-
-        return new ForkJoinPool(
-                50,
-                ForkJoinPool.defaultForkJoinWorkerThreadFactory,
-                null,
-                false,
-                50,
-                60,
-                1,
-                null,
-                60L,
-                TimeUnit.SECONDS);
     }
 }
