f5be3a0914761cbf298e2494caec797808e547ae
drovocek <mycodepg@gmail.com>
2023-07-04 10:57:12 +0300

    done

--- src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
@@ -2,24 +2,25 @@ package course.concurrency.exams.auction;
 
 public class AuctionOptimistic implements Auction {
 
-    private Notifier notifier;
+    private final Notifier notifier;
 
     public AuctionOptimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
+    private volatile Bid latestBid = new Bid(0L, 0L, 0L);
 
     public boolean propose(Bid bid) {
         if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
             latestBid = bid;
+            notifier.sendOutdatedMessage(bid);
             return true;
         }
         return false;
     }
 
     public Bid getLatestBid() {
+
         return latestBid;
     }
 }
