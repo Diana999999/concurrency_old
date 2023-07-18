e9f40aff492d0331d9ba14f9438fc76e40b8b09f
drovocek <mycodepg@gmail.com>
2023-07-18 09:21:56 +0300

    done

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,19 +1,24 @@
 package course.concurrency.m3_shared.collections;
 
-import java.util.HashSet;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toSet;
 
 public class RestaurantService {
 
-    private Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
+    private final Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
         put("A", new Restaurant("A"));
         put("B", new Restaurant("B"));
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private final Queue<String> stat = new LinkedBlockingQueue<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +26,14 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.add(restaurantName);
     }
 
     public Set<String> printStat() {
-        // your code
-        return new HashSet<>();
+        return stat.stream()
+                .collect(groupingBy(identity()))
+                .entrySet().stream()
+                .map(entry -> entry.getKey() + " - " + entry.getValue().size())
+                .collect(toSet());
     }
 }
