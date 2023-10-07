96c6ecdb321c56056277c6872f048d10957df407
rmqh <haqmar@gmail.com>
2023-10-07 13:37:43 +0300

    Add hw0_intro

--- src/test/java/course/concurrency/m0_intro/WelcomeClassBenchmarks.java
+++ src/test/java/course/concurrency/m0_intro/WelcomeClassBenchmarks.java
@@ -1,6 +1,11 @@
 package course.concurrency.m0_intro;
 
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
